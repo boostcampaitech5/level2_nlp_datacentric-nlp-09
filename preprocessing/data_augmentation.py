57e5d88d43297db351ffd48d7112ae7529de41ba
jys8332 <bbogeul123@naver.com>
2023-05-26 12:02:24 +0000

    fix: swap text function
    - able to concat augmented data to raw data
    
    #7

--- preprocessing/data_augmentation.py
+++ preprocessing/data_augmentation.py
@@ -7,10 +7,14 @@ def temp_aug(df):
 
 
 def swap_text_aug(df):
+    new_df = None
+    new_df = df[df['input_text'].str.contains("…")]
+
     def swap_text(sentence):
         segment = re.split(r"(…)", sentence)
-        segment.reverse()
+        if len(segment) > 2:
+            segment.reverse()
         sentence = "".join(segment)
         return sentence
-    df['input_text'] = df['input_text'].apply(lambda x: swap_text(x))
-    return df
+    new_df['input_text'] = new_df['input_text'].apply(lambda x: swap_text(x))
+    return new_df
